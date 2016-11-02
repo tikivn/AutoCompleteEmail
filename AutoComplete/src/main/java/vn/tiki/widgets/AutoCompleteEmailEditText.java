82e12a9a14bf79f67c823924f7cc01f13331deb2
Ken Zira <shaymin.android@gmail.com>
2016-11-02 22:22:55 +0700

    init commit

---
+++ AutoComplete/src/main/java/vn/tiki/widgets/AutoCompleteEmailEditText.java
@@ -0,0 +1,138 @@
+package vn.tiki.widgets;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.v7.widget.AppCompatEditText;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by KenZira on 6/30/16.
+ */
+public class AutoCompleteEmailEditText extends AppCompatEditText {
+
+    private List<String> domains;
+
+    private boolean isBackPressing;
+
+
+    public AutoCompleteEmailEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    public AutoCompleteEmailEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        TypedArray a = context.obtainStyledAttributes(attrs,
+                R.styleable.AutoCompleteEmailEditText);
+
+        try {
+            CharSequence[] arrayOfCharSequences = a.getTextArray(R.styleable.AutoCompleteEmailEditText_domains);
+
+            if (arrayOfCharSequences != null) {
+
+                String[] referencedDomains = new String[arrayOfCharSequences.length];
+                int i = 0;
+
+                for (CharSequence ch : arrayOfCharSequences) {
+                    referencedDomains[i++] = ch.toString();
+                }
+
+                domains = Arrays.asList(referencedDomains);
+            } else {
+                domains = new ArrayList<>();
+            }
+
+        } finally {
+            a.recycle();
+        }
+
+        init();
+    }
+
+    public AutoCompleteEmailEditText(Context context) {
+        super(context);
+        init();
+    }
+
+    private TextWatcher watcher = new TextWatcher() {
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            isBackPressing = count > 0 && after == 0;
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+            if (!isBackPressing) {
+
+                String textAfterChange = s.toString();
+                String matchDomain = "";
+                String textAfterAtSymbol = "";
+
+                int atSignPosition = textAfterChange.indexOf('@');
+                if (atSignPosition > 1
+                        && atSignPosition < (textAfterChange.length() + 1)) {
+
+
+                    textAfterAtSymbol = textAfterChange.substring(atSignPosition + 1, textAfterChange.length());
+
+                    if (textAfterAtSymbol.isEmpty())
+                        return;
+
+                    for (String domain : domains) {
+
+                        if (domain.startsWith(textAfterAtSymbol)) {
+                            matchDomain = domain;
+                        }
+                    }
+                }
+
+                removeTextChangedListener(watcher);
+
+                if (!matchDomain.isEmpty()) {
+                    String filled = matchDomain.substring(textAfterAtSymbol.length(), matchDomain.length());
+                    String text = textAfterChange + filled;
+                    int highlight = text.lastIndexOf(filled);
+                    setText(text);
+                    setSelection(highlight, text.length());
+                }
+                addTextChangedListener(watcher);
+            }
+        }
+
+    };
+
+    public void init() {
+        setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+        addTextChangedListener(watcher);
+        setOnFocusChangeListener(new OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus){
+                    if(getSelectionStart() != getSelectionEnd()){
+                        setSelection(getText().length());
+                    }
+                }
+            }
+        });
+    }
+
+    public void setSuggestedDomains(List<String> domains) {
+        this.domains = domains;
+    }
+
+}
