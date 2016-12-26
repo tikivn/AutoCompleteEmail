29bfb361a3e1de1b712a935b48a2c0fe0ab1e000
Ken Zira <shaymin.android@gmail.com>
2016-12-26 21:30:35 +0700

    rename variables

--- library/src/main/java/vn/tiki/widgets/AutoCompleteEmailEditText.java
+++ library/src/main/java/vn/tiki/widgets/AutoCompleteEmailEditText.java
@@ -7,8 +7,8 @@ import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -18,121 +18,131 @@ import java.util.List;
  */
 public class AutoCompleteEmailEditText extends AppCompatEditText {
 
-    private List<String> domains;
+  private static final String TAG = "AutoCompleteEmailEdit";
+  private List<String> domains;
+  private boolean isBackPressing;
+  private final TextWatcher watcher = new TextWatcher() {
 
-    private boolean isBackPressing;
-
-
-    public AutoCompleteEmailEditText(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        init();
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+      Log.d(
+          TAG,
+          "beforeTextChanged() called with: s = ["
+              + s
+              + "], start = ["
+              + start
+              + "], count = ["
+              + count
+              + "], after = ["
+              + after
+              + "]");
+      isBackPressing = count > 0 && after == 0;
     }
 
-    public AutoCompleteEmailEditText(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        TypedArray a = context.obtainStyledAttributes(attrs,
-                R.styleable.AutoCompleteEmailEditText);
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
 
-        try {
-            CharSequence[] arrayOfCharSequences = a.getTextArray(R.styleable.AutoCompleteEmailEditText_domains);
+    }
 
-            if (arrayOfCharSequences != null) {
+    @Override
+    public void afterTextChanged(Editable s) {
 
-                String[] referencedDomains = new String[arrayOfCharSequences.length];
-                int i = 0;
+      if (!isBackPressing) {
 
-                for (CharSequence ch : arrayOfCharSequences) {
-                    referencedDomains[i++] = ch.toString();
-                }
+        String textAfterChange = s.toString();
+        String matchDomain = "";
+        String textAfterAtSymbol = "";
 
-                domains = Arrays.asList(referencedDomains);
-            } else {
-                domains = new ArrayList<>();
+        int atSignPosition = textAfterChange.indexOf('@');
+        if (atSignPosition > 1
+            && atSignPosition < (textAfterChange.length() + 1)) {
+
+          textAfterAtSymbol = textAfterChange.substring(
+              atSignPosition + 1,
+              textAfterChange.length());
+
+          if (textAfterAtSymbol.isEmpty()) {
+            return;
+          }
+
+          for (String domain : domains) {
+
+            if (domain.startsWith(textAfterAtSymbol)) {
+              matchDomain = domain;
             }
-
-        } finally {
-            a.recycle();
+          }
         }
 
-        init();
-    }
+        removeTextChangedListener(watcher);
 
-    public AutoCompleteEmailEditText(Context context) {
-        super(context);
-        init();
-    }
-
-    private TextWatcher watcher = new TextWatcher() {
-
-        @Override
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            isBackPressing = count > 0 && after == 0;
+        if (!matchDomain.isEmpty()) {
+          String filled = matchDomain.substring(textAfterAtSymbol.length(), matchDomain.length());
+          String text = textAfterChange + filled;
+          int highlight = text.lastIndexOf(filled);
+          setText(text);
+          setSelection(highlight, text.length());
         }
-
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-        }
-
-        @Override
-        public void afterTextChanged(Editable s) {
-
-            if (!isBackPressing) {
-
-                String textAfterChange = s.toString();
-                String matchDomain = "";
-                String textAfterAtSymbol = "";
-
-                int atSignPosition = textAfterChange.indexOf('@');
-                if (atSignPosition > 1
-                        && atSignPosition < (textAfterChange.length() + 1)) {
-
-
-                    textAfterAtSymbol = textAfterChange.substring(atSignPosition + 1, textAfterChange.length());
-
-                    if (textAfterAtSymbol.isEmpty())
-                        return;
-
-                    for (String domain : domains) {
-
-                        if (domain.startsWith(textAfterAtSymbol)) {
-                            matchDomain = domain;
-                        }
-                    }
-                }
-
-                removeTextChangedListener(watcher);
-
-                if (!matchDomain.isEmpty()) {
-                    String filled = matchDomain.substring(textAfterAtSymbol.length(), matchDomain.length());
-                    String text = textAfterChange + filled;
-                    int highlight = text.lastIndexOf(filled);
-                    setText(text);
-                    setSelection(highlight, text.length());
-                }
-                addTextChangedListener(watcher);
-            }
-        }
-
-    };
-
-    public void init() {
-        setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
         addTextChangedListener(watcher);
-        setOnFocusChangeListener(new OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(!hasFocus){
-                    if(getSelectionStart() != getSelectionEnd()){
-                        setSelection(getText().length());
-                    }
-                }
-            }
-        });
+      }
+    }
+  };
+
+  public AutoCompleteEmailEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+    init();
+  }
+
+  public AutoCompleteEmailEditText(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    TypedArray a = context.obtainStyledAttributes(
+        attrs,
+        R.styleable.AutoCompleteEmailEditText);
+
+    try {
+      CharSequence[] domainsFromResource =
+          a.getTextArray(R.styleable.AutoCompleteEmailEditText_domains);
+
+      if (domainsFromResource != null) {
+
+        String[] referencedDomains = new String[domainsFromResource.length];
+        int i = 0;
+
+        for (CharSequence domain : domainsFromResource) {
+          referencedDomains[i++] = domain.toString();
+        }
+
+        domains = Arrays.asList(referencedDomains);
+      } else {
+        domains = new ArrayList<>();
+      }
+    } finally {
+      a.recycle();
     }
 
-    public void setSuggestedDomains(List<String> domains) {
-        this.domains = domains;
-    }
+    init();
+  }
 
+  public AutoCompleteEmailEditText(Context context) {
+    super(context);
+    init();
+  }
+
+  public void init() {
+    setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+    addTextChangedListener(watcher);
+    setOnFocusChangeListener(new OnFocusChangeListener() {
+      @Override
+      public void onFocusChange(View v, boolean hasFocus) {
+        if (!hasFocus) {
+          if (getSelectionStart() != getSelectionEnd()) {
+            setSelection(getText().length());
+          }
+        }
+      }
+    });
+  }
+
+  public void setSuggestedDomains(List<String> domains) {
+    this.domains = domains;
+  }
 }
